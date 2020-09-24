deb627949db983cac39468185fe2ec8a124b63d8
Daniel Baulé <dsbaule@gmail.com>
2020-09-24 17:37:18 -0300

    Simulado 2 - Uniao de conjuntos

--- Simulado 02/UniaoDeConjuntos/src/UniaoDeConjuntos/No.py
+++ Simulado 02/UniaoDeConjuntos/src/UniaoDeConjuntos/No.py
@@ -1,24 +1,41 @@
+""" 
+Autor: Daniel de Souza Baulé (16200639)
+
+Disciplina: INE5452 - Topicos Especiais em Algoritmos II
+Atividade:  Segundo simulado - Questoes extra-URI
+
+União de Conjuntos
+"""
+
+# Class to implement union find Node
 class No:
+    # New node has given value, rank 0 and is it's own father
     def __init__(self, valor):
         self.rank = 0
         self.valor = valor
         self.pai = self
     
+    # Find root
     def encontrar(self):
+        # If not root, make root of father new father
         if self.pai != self:
             self.pai = self.pai.encontrar()
+        # Return new father
         return self.pai
 
-    def unir (self, no):
+    # Union with another node
+    def unir (self, no: "No"):
+        # Get roots
         root_self = self.encontrar()
         root_no = no.encontrar()
-
+        # If they have the same root, already united
         if root_self == root_no:
             return
-        
+        # Make higher ranked one new root  
         if root_self.rank > root_no.rank:
             root_no.pai = root_self
         else:
             root_self.pai = root_no
+            # If ranks were equal, increase new root rank
             if root_self.rank == root_no.rank:
                 root_no.rank += 1
--- Simulado 02/UniaoDeConjuntos/src/__main__.py
+++ Simulado 02/UniaoDeConjuntos/src/__main__.py
@@ -1,13 +1,53 @@
+""" 
+Autor: Daniel de Souza Baulé (16200639)
+
+Disciplina: INE5452 - Topicos Especiais em Algoritmos II
+Atividade:  Segundo simulado - Questoes extra-URI
+
+União de Conjuntos
+"""
+
 from src.UniaoDeConjuntos.No import No
+from pprint import pp
 
+# Get input sets
 n = int(input("Número de Conjuntos: "))
-
 conjuntos = list()
-
 for conjunto in range(n):
     conjuntos.append([int(x) for x in input(f"Conjunto {conjunto + 1}: ").split()])
 
-m = int(input("Número de Operações: "))
+# For each set, create nodes for all elements and unite them
+conjunto_dict = dict()
+for conjunto in conjuntos:
+    conjunto_dict[conjunto[0]] = No(conjunto[0])
+    for no in conjunto[1:]:
+        conjunto_dict[no] = No(no)
+        conjunto_dict[no].unir(conjunto_dict[conjunto[0]])
 
+# Get input values and execute new unions
+m = int(input("Número de Operações: "))
 for operacao in range(m):
     i, j = [int(x) for x in input(f"Operação {operacao + 1}: ").split()]
+    conjunto_dict[i].unir(conjunto_dict[j])
+
+# Go through all nodes, grouping them by root to obtain sets
+final_sets = dict()
+for no in conjunto_dict.values():
+    no_raiz = no.encontrar()
+    set_conjunto = final_sets.get(no_raiz, None)
+    if set_conjunto is None:
+        final_sets[no_raiz] = {no.valor}
+    else:
+        final_sets[no_raiz].add(no.valor)
+
+# Sort each set
+sorted_sets = [list(final_set) for final_set in final_sets.values()]
+for sorted_set in sorted_sets:
+    sorted_set.sort()
+# Sort sets in relation to each other based on first element
+sorted_sets.sort()
+
+# Print resulting sets
+print("Conjuntos Resultantes:")
+for sorted_set in sorted_sets:
+    print(*sorted_set, sep=' ')
