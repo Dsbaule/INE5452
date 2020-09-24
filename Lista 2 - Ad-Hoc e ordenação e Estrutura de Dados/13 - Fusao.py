c1319c35f78ce6d11b9c0bd8f997663238a6f7c3
Daniel Baulé <dsbaule@gmail.com>
2020-09-24 16:44:00 -0300

    13 - Fusao em python

--- Lista 2 - Ad-Hoc e ordenação e Estrutura de Dados/13 - Fusao.py
+++ Lista 2 - Ad-Hoc e ordenação e Estrutura de Dados/13 - Fusao.py
@@ -1,46 +1,31 @@
-class Pointer:
-    def __init__(self, value):
-        self.value = value
-        self.final = True
-    def updateValue(self, value):
-        if self.final:
-            self.value = value
-        else:
-            self.value.updateValue(value)
-    def update(self, value):
-        if self.final:
-            self.final = False
-            while not value.final:
-                value = value.getPointer()
-            self.value = value
-        else:
-            self.value.update(value)
-    def get(self):
-        if self.final:
-            return self.value
-        else:
-            return self.value.get()
-    def getPointer(self):
-        return self.value
-
+def getAndUpdate(bancos, index):
+    if bancos[index] == index:
+        return index
+    else:
+        bancos[index] = getAndUpdate(bancos, bancos[index])
+        return bancos[index]
 
 def main():
     n, b = [int(x) for x in input().split()]
 
     bancos = list()
 
-    for i in range(n):
-        bancos.append(Pointer([x == i for x in range(n)]))
+    bancos.append(n)
+
+    for i in range(1, n + 1):
+        bancos.append(i)
 
     for _ in range(b):
         c, b1, b2 = input().split()
-        b1 = int(b1) - 1
-        b2 = int(b2) - 1
+        b1 = int(b1)
+        b2 = int(b2)
 
         if c == 'F':
-            bancos[b1].updateValue([bancos[b1].get()[i] or bancos[b2].get()[i] for i in range(n)])
-            bancos[b2].update(bancos[b1])
-        elif bancos[b1].get()[b2]:
+            if getAndUpdate(bancos, b1) >= getAndUpdate(bancos, b2):
+                bancos[bancos[b1]] = bancos[b2]
+            else:
+                bancos[bancos[b2]] = bancos[b1]
+        elif getAndUpdate(bancos, b1) == getAndUpdate(bancos, b2):
             print('S')
         else:
             print('N')
