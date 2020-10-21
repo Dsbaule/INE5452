123c621494ee8ae7b8cf23ceb49af9a2c8a645e7
Daniel Baulé <dsbaule@gmail.com>
2020-10-20 21:58:15 -0300

    Commented

--- Simulado 03/Karatsuba/src/__main__.py
+++ Simulado 03/Karatsuba/src/__main__.py
@@ -45,22 +45,24 @@ def karatsuba_rec(n1, n2):
         #print('K {} {} - {}'.format(n1, n2, result))
         return result
     
+    # Obtem a metade do menor numero
     m = min(n1.bit_length(), n2.bit_length())
     m2 = m // 2
 
+    # Divide os dois numeros
     high1, low1 = split_by_mask(n1, m2)
     high2, low2 = split_by_mask(n2, m2)
 
+    # Calcula os valores parciais com o Algoritmo de Karatsuba
     z0 = karatsuba_rec(low1, low2)
     z1 = karatsuba_rec((low1 + high1), (low2 + high2))
     z2 = karatsuba_rec(high1, high2)
 
-
+    # Calcula o valor total
     result = (z2 * (2 ** (m2 * 2))) + ((z1 - z2 - z0) * (2 ** m2)) + z0
-    #print('K {} {} - {}'.format(n1, n2, result))
+
+    # Retorna o resultado
     return result
-
-
 
 # Algorítmo de Karatsuba com ED (vetor)
 def karatsuba(n1, n2, rec=False):
@@ -78,55 +80,68 @@ def karatsuba(n1, n2, rec=False):
 
 # Algoritmo de Karatsuba com ED
 def karatsuba_ed(n1, n2, l1, len1, l2, len2):
+    # Caso o numero tenha menos de 5 bits, apenas multiplique
     if len1 < 5 or len2 < 5:
         return multiplica(n1, n2, l1, len1, l2, len2)
 
+    # Obtem a metade do menor numero
     m = min(len1, len2)
     m2 = m // 2
 
+    # Calcula os valores parciais com o Algoritmo de Karatsuba
+    # Multiplicacao dos bits menos significativos
     z0 = karatsuba_ed(n1, n2, l1, m2, l2, m2)
 
+    # Calcula a soma dos menos significativos com os mais significativos
     sum1 = soma(n1, n1, l1, m2, l1 + m2, len1 - m2)
     sum2 = soma(n2, n2, l2, m2, l2 + m2, len2 - m2)
+
+    # Multiplicacao das somas
     z1 = karatsuba(sum1, sum2)
 
+    # Multiplicacao dos bits mmais significativos
     z2 = karatsuba_ed(n1, n2, l1 + m2, len1 - m2, l2 + m2, len2 - m2)
 
+    # Calcula o  resultaddo
     result = (z2 * (2 ** (m2 * 2))) + ((z1 - z2 - z0) * (2 ** m2)) + z0
-    #print('K {} {} {} {} {} {} - {}'.format(n1[l1:l1+len1], l1, len1, n2[l2:l2+len2], l2, len2,result))
+
+    # Retorna o resultado
     return result
 
 
 def multiplica(n1: list(), n2: list(), l1, len1, l2, len2):
-    num1 = 0
-    num2 = 0
+    num1 = 0; num2 = 0
 
+    # Calcula o segundo inteiro    
     for i in range(len1):
         num1 += (2 ** i) * n1[l1 + i]
-    
+
+    # Calcula o segundo inteiro    
     for i in range(len2):
         num2 += (2 ** i) * n2[l2 + i]
 
-    #print('M {} {} {} {} {} {} - {}'.format(n1[l1:l1+len1], l1, len1, n2[l2:l2+len2], l2, len2,num1 * num2))
+    # Retorna a multiplicacao dos inteiros
     return num1 * num2
 
 def soma(n1: list(), n2: list(), l1, len1, l2, len2):
-    num1 = 0
-    num2 = 0
+    num1 = 0; num2 = 0
 
+    # Calcula o segundo inteiro    
     for i in range(len1):
         num1 += (2 ** i) * n1[l1 + i]
-    
+
+    # Calcula o segundo inteiro    
     for i in range(len2):
         num2 += (2 ** i) * n2[l2 + i]
 
-    #print('S {} {} {} {} {} {} - {}'.format(n1[l1:l1+len1], l1, len1, n2[l2:l2+len2], l2, len2,num1 + num2))
+    # Retorna a multiplicacao dos inteiros
     return num1 + num2
 
 def main():
     # Obtem o número a ser multiplicado
     n1 = int(input("n1 = "))
     n2 = int(input("n2 = "))
-    print('Resultado da Multiplicacao: {}'.format(karatsuba(n1, n2, False)))
+    # Imprime o resultado da multiplicação
+    print('Resultado da Multiplicacao: {}'.format(karatsuba(n1, n2)))
 
 main()
